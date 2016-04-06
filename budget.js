175ed13a4d2ae962797aade17205c1a2369bc8bd
José Padilla <jpadilla@webapplicate.com>
2016-04-06 12:43:14 -0400

    Add eventbrite processing fees

--- budget.js
+++ budget.js
@@ -5,11 +5,12 @@ process.exit(
           // for details, see our accountant:
           // https://travis-ci.org/fullstacknights/fullstacknights.github.io
 
-          // Fullstack Nights v3, 2016-03-29 at @prsciencetrust
-          // featuring @pedrocruzvr and @_xiomara7
+// Fullstack Nights v3, 2016-03-29 at @prsciencetrust
+// featuring @pedrocruzvr and @_xiomara7
 
-+  385.00 // ticket sales  @eventbrite
++   385.00     // ticket sales    @eventbrite
 
--  110.95 // pizzas        @costco
--   29.92 // refreshments  SuperMax
+-    97.79     // processing fees @eventbrite
+-   110.95     // pizzas          @costco
+-    29.92     // refreshments    SuperMax
 );
