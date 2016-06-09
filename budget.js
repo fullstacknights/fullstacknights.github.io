f59e85319d5f90e3b32175f10e633f55c7fbd464
José Padilla <jpadilla@webapplicate.com>
2016-06-09 16:42:03 -0400

    Update budget.js

--- budget.js
+++ budget.js
@@ -13,4 +13,12 @@ process.exit(
 -    97.79     // processing fees @eventbrite
 -   110.95     // pizzas          @costco
 -    29.92     // refreshments    SuperMax
+
+// Fullstack Nights v4, 2016-05-23 at @prsciencetrust
+// featuring @theavatare and @Makobi93
+
++   305.00     // ticket sales    @eventbrite
+
+-    77.47     // processing fees @eventbrite
+-   133.00     // pizzas          @costco
 );
