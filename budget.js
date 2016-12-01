601208824e9068fa6eff3ad7b482d9dbb41d6cbb
José Padilla <jpadilla@webapplicate.com>
2016-12-01 16:50:53 -0500

    Update budget.js

--- budget.js
+++ budget.js
@@ -8,19 +8,19 @@ process.exit(
 // Fullstack Nights v3, 2016-03-29 at @prsciencetrust
 // featuring @pedrocruzvr and @_xiomara7
 
-+   385.00      // ticket sales    @eventbrite
++   385.00      // ticket sales     @eventbrite
 
--    97.79      // processing fees @eventbrite
--   110.95      // pizzas          @costco
--    29.92      // refreshments    SuperMax
+-    97.79      // processing fees  @eventbrite
+-   110.95      // pizzas           @costco
+-    29.92      // refreshments     SuperMax
 
 // Fullstack Nights v4, 2016-05-23 at @prsciencetrust
 // featuring @theavatare and @Makobi93
 
-+   305.00      // ticket sales    @eventbrite
++   305.00      // ticket sales     @eventbrite
 
--    77.47      // processing fees @eventbrite
--   133.00      // pizzas          @costco
+-    77.47      // processing fees  @eventbrite
+-   133.00      // pizzas           @costco
 
 // Fullstack Nights v5, 2016-07-26 at @prsciencetrust
 // featuring @gcollazo and @nexomorales
@@ -29,7 +29,7 @@ process.exit(
 
 -  59.69        // processing fees  @eventbrite
 - 110.95        // pizzas           @costco
--  29.92        // refreshments    SuperMax
+-  29.92        // refreshments     SuperMax
 
 // Fullstack Nights v6, 2016-11-28 at @prsciencetrust
 // featuring @elbuo8 and @Carlos_Perez
@@ -38,5 +38,6 @@ process.exit(
 
 -  67.31        // processing fees  @eventbrite
 - 110.95        // pizzas           @costco
--  12.00        // refreshments    SuperMax
+-  12.48        // refreshments     SuperMax
+-  30.00        // ads              @facebook
 );
