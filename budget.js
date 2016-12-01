c7639af0fa8735b931e2ca746b1a546892083a40
José Padilla <jpadilla@webapplicate.com>
2016-12-01 16:45:01 -0500

    Update budget.js

--- budget.js
+++ budget.js
@@ -1,34 +1,42 @@
 process.exit(
-     0.00 // budget.js - keeping the books for @fullstacknights
-          // Inspired by @brooklyn_js
+     0.00       // budget.js - keeping the books for @fullstacknights
+                // Inspired by @brooklyn_js
 
-          // for details, see our accountant:
-          // https://travis-ci.org/fullstacknights/fullstacknights.github.io
+                // for details, see our accountant:
+                // https://travis-ci.org/fullstacknights/fullstacknights.github.io
 
 // Fullstack Nights v3, 2016-03-29 at @prsciencetrust
 // featuring @pedrocruzvr and @_xiomara7
 
-+   385.00     // ticket sales    @eventbrite
++   385.00      // ticket sales    @eventbrite
 
--    97.79     // processing fees @eventbrite
--   110.95     // pizzas          @costco
--    29.92     // refreshments    SuperMax
+-    97.79      // processing fees @eventbrite
+-   110.95      // pizzas          @costco
+-    29.92      // refreshments    SuperMax
 
 // Fullstack Nights v4, 2016-05-23 at @prsciencetrust
 // featuring @theavatare and @Makobi93
 
-+   305.00     // ticket sales    @eventbrite
++   305.00      // ticket sales    @eventbrite
 
--    77.47     // processing fees @eventbrite
--   133.00     // pizzas          @costco
+-    77.47      // processing fees @eventbrite
+-   133.00      // pizzas          @costco
 
 // Fullstack Nights v5, 2016-07-26 at @prsciencetrust
 // featuring @gcollazo and @nexomorales
 
-+ 240.00       // ticket sales     @eventbrite
++ 240.00        // ticket sales     @eventbrite
 
-- 59.69        // processing fees  @eventbrite
-- 110.95       // pizzas           @costco
--  29.92       // refreshments    SuperMax
+-  59.69        // processing fees  @eventbrite
+- 110.95        // pizzas           @costco
+-  29.92        // refreshments    SuperMax
 
+// Fullstack Nights v6, 2016-11-28 at @prsciencetrust
+// featuring @elbuo8 and @Carlos_Perez
+
++ 265.00        // ticket sales     @eventbrite
+
+-  67.31        // processing fees  @eventbrite
+- 110.95        // pizzas           @costco
+-  12.00        // refreshments    SuperMax
 );
